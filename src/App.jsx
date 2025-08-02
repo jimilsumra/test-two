c1dacf967f571d6c22d06072e304bb7cb1ae1993
Jimil <jimilsumra@gmail.com>
2025-08-02 22:17:42 +1200

    update useEffect and with update date object

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,42 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+
+  const [time,setTime] = useState(new Date());
+
+  useEffect(()=>{
+
+    setInterval(()=>{
+      setTime(new Date());
+    }, 1000);
+
+  });
+
+  const hours = time.getHours();
+  const min = time.getMinutes();
+  const sec = time.getSeconds();
+
+
+  /*
   const dateObj = new Date();
   const [hours,setHours] = useState('');
   const [min,setMin] = useState('');
   const [sec,setSec] = useState('');
-  setInterval(()=>{
-    //console.log('called');
-    setHours(dateObj.getHours());
-    setMin(dateObj.getMinutes());
-    setSec(dateObj.getSeconds());
-    
-  /*hours = dateObj.getHours();
-   min = dateObj.getMinutes();
-   sec = dateObj.getSeconds();*/
-  },1000);
+  useEffect(() =>  {
+    setInterval(()=>{
+      //clearInterval(200);
+      //console.log('called');
+      setHours(dateObj.getHours());
+      setMin(dateObj.getMinutes());
+      setSec(dateObj.getSeconds());
+      
+    },1000);
+  },[]);
+
+  */
   //console.log(dateObj.getHours());
 
   return (
